124d2d3cb4ae1efb0c3f77d52e5a9a63f89020db
Jie Hu <jhu9@ualberta.ca>
2025-01-04 15:21:32 -0700

    update2

--- readStachiwTable.py
+++ readStachiwTable.py
@@ -94,7 +94,7 @@ print(u"press \'a\': spring/autumn (20\N{DEGREE SIGN}C)")
 print(u"press \'b\': summer (30\N{DEGREE SIGN}C)")
 print(u"press \'c\': winter (5\N{DEGREE SIGN}C)")
 print(u"or enter a temperature value: (in \N{DEGREE SIGN}C)")
-choice_temp = input("press:")
+choice_temp = input("press/enter:")
 room_temp = 27 ## default value
 if choice_temp == 'a':
    room_temp = 20
@@ -108,7 +108,12 @@ elif choice_temp == 'c':
 elif choice_temp =="":
    print(u"room temperature is set to 27\N{DEGREE SIGN}C by default")  
 else:
-   room_temp = ceil(choice_temp) 
+   room_temp = ceil( int(choice_temp) )
+   if room_temp>70 or room_temp<-70:
+       print("Can you survive this room temperature? Please enter again, range from -70 to 70 degC")
+       while room_temp>70 or room_temp<-70:
+           choice_temp = input("Enter:")
+           room_temp = ceil( int(choice_temp) )
    print("room temperature is set to", room_temp, u"\N{DEGREE SIGN}C.")
 
 print("*************************************")
@@ -180,10 +185,13 @@ max_coolingRate_to27degC = fahrenheit_to_celsius_rate(max_coolingRate_to80F)
 decreasetime_to27degC    = list_decreasetime_to27degC[row_index]
 totaltime                = list_totaltime[row_index]
 
-print("!!!!! max_heatingRate_degC",round(max_heatingRate_degC,3) ,"suggested", round(suggest_heatingRate_degC,3))
+max_heatingRate_degC     = round(max_heatingRate_degC,3)
+coolingRate_to110degC    = round(coolingRate_to110degC,3)
+max_coolingRate_to27degC = round(max_coolingRate_to27degC,3)
+print("!!!!! max_heatingRate_degC", max_heatingRate_degC, "suggested", suggest_heatingRate_degC)
 #print( 140. - room_temp, heatingRate_degC, actual_risetime_to140degC)
-print("cooling-to-110degC:", round(coolingRate_to110degC,3))
-print("cooling-to-roomTemp:", round(max_coolingRate_to27degC,3))
+print("cooling-to-110degC:", coolingRate_to110degC)
+print("cooling-to-roomTemp:", max_coolingRate_to27degC)
 
 print(u"Note: the Stachiw's tables assume a default room temperature 27\N{DEGREE SIGN}C; here we use "+ str(room_temp) + "\N{DEGREE SIGN}C.")
 print("----------------------------------------------------------------")
@@ -210,32 +218,32 @@ print(str(round(actual_risetime_to140degC,2)) + " hours; or " + str(round(actual
 
 actual_totaltime = (140. - room_temp)/suggest_heatingRate_degC + holdTime_at140degC + (140. - 110.)/coolingRate_to110degC + holdTime_at110degC + (110. - room_temp)/max_coolingRate_to27degC
 
-print("----------------------------------")
+print("------------------------------------------------------")
 print(u"Hold time at 140 \N{DEGREE SIGN}C:")
 print( str(round(holdTime_at140degC,2)) + " hours; or "+ str(round(holdTime_at140degC*60,2)) + " minutes")
-print("----------------------------------")
+print("------------------------------------------------------")
 print(u"Approx. cooling rate to 110 \N{DEGREE SIGN}C:")
 print( str(round(coolingRate_to110degC,2)) + u"\N{DEGREE SIGN}C/hour; or " + str(round(coolingRate_to110degC/60,2)) + u"\N{DEGREE SIGN}C/minute")
 print( "or "+str(round(coolingRate_to230F,2)) + u"\N{DEGREE SIGN}F/hour")
-print("----------------------------------")
+print("------------------------------------------------------")
 print(u"Hours to Cool oven to 110 \N{DEGREE SIGN}C:")
 print( str( round(decreasetime_to110degC,2))+ " hours; or "+ str(round(decreasetime_to110degC*60,2)) + " minutes")
-print("----------------------------------")
+print("------------------------------------------------------")
 print(u"Hold time at 110 \N{DEGREE SIGN}C:")
 print( str(round(holdTime_at110degC,2)) + " hours")
-print("----------------------------------")
+print("------------------------------------------------------")
 print(u"Max cooling rate to room temperature:")
 print( str(round(max_coolingRate_to27degC,2)) + u"\N{DEGREE SIGN}C/hour; or " + str(round(max_coolingRate_to27degC/60,2)) + u"\N{DEGREE SIGN}C/minute") 
 print( "or "+str(round(max_coolingRate_to80F,2)) + u"\N{DEGREE SIGN}F/hour")
-print("----------------------------------")
+print("------------------------------------------------------")
 print("Total time expected in Stachiw's table:")
 print(str(totaltime) + " hours; or " + str( round(totaltime*60, 2) )+ " minutes")
 print("Total time suggested:")
 print(str(round(actual_totaltime,1)) + " hours; or " + str( np.ceil(actual_totaltime*60) )+ " minutes")
 actual_days = actual_totaltime/24
-if actual_totaltime>30:
+if actual_totaltime>48:
     print("or " + str( int(actual_days) ) + " days and " + str( round(actual_totaltime - int(actual_days)*24, 1) ) + " hours.")
-
+print("------------------------------------------------------")
 ## print the footnotes
 df_note = pd.read_csv(file_path, skiprows=2)
 footnotes = df_note.tail(3).iloc[:, 0]
@@ -373,36 +381,37 @@ rootCode_strings = [
 "",
 "// Function to represent stachiwCycle",
 "double stachiwCycle1(double t) {",
-"    double room_temp = 27;",
-"    double timeRegion1 = (140.0 - room_temp) / 30.0;",
-"    double timeRegion2 = timeRegion1 + 14.0;  // Hold for 14 hours at 140degC",
-"    double timeRegion3 = timeRegion2 + (140.0 - 110.0) / std::abs(-13.33);",
-"    double timeRegion4 = timeRegion3 + 7.0;   // Hold for 7 hours at 110degC",
-"    double timeRegion5 = timeRegion4 + (110.0 - room_temp) / std::abs(-12.22);",
+"    double room_temp = "+ str(room_temp) + ";",
+"    double timeRegion1 = (140.0 - room_temp)/" + str(heatingRate_degC) + ";",
+"    double timeRegion2 = timeRegion1 + " + str(holdTime_at140degC) + ";  // Hold for " + str(holdTime_at140degC) + " hours at 140degC",
+"    double timeRegion3 = timeRegion2 + (140.0 - 110.0)/std::abs(" + str(coolingRate_to110degC) + ");",
+"    double timeRegion4 = timeRegion3 + " + str(holdTime_at110degC) +  ";   // Hold for " + str(holdTime_at110degC) + " hours at 110degC",
+"    double timeRegion5 = timeRegion4 + (110.0 - room_temp)/std::abs(" + str(max_coolingRate_to27degC) + ");",
 "",
 "    if (0 <= t && t <= timeRegion1) {",
-"        return 20.0 * t + room_temp;",
+"        return " + str(heatingRate_degC) + "* t + room_temp;",
 "    } else if (timeRegion1 < t && t <= timeRegion2) {",
 "        return 140.0;",
 "    } else if (timeRegion2 < t && t <= timeRegion3) {",
-"        return 140.0 + -13.33 * (t - timeRegion2);",
+"        return 140.0 + -1*" + str(coolingRate_to110degC) + "*(t - timeRegion2);",
 "    } else if (timeRegion3 < t && t <= timeRegion4) {",
 "        return 110.0;",
 "    } else if (timeRegion4 <= t && t < timeRegion5) {",
-"        return 110.0 + -12.22 * (t - timeRegion4);",
+"        return 110.0 + -1*" + str(max_coolingRate_to27degC) + "*(t - timeRegion4);",
 "    } else {",
-"        return NAN;  // Return NaN if t is out of bounds",
+"        return 0;  // Return 0 if t is out of bounds",
 "    }",
 "}",
 "",
 "void plotStachiwCycle() {",
 "    // Time vectors for the different cycles",
-"    int nPoints = 400;",
+"    int endTime = " + str( int(actual_totaltime) ) + "; // total time", 
+"    int nPoints = endTime*100;",
 "    std::vector<double> time_stachiwCycle1(nPoints);",
 "    std::vector<double> temperature_stachiwCycle1(nPoints);",
 "    ",
 "    for (int i = 0; i < nPoints; ++i) {",
-"        time_stachiwCycle1[i] = i;  // Fill the time values",
+"        time_stachiwCycle1[i] = i*0.01;  // Fill the time values",
 "        temperature_stachiwCycle1[i] = stachiwCycle1(time_stachiwCycle1[i]);",
 "    }",
 "",
@@ -411,8 +420,10 @@ rootCode_strings = [
 "",
 "    // Create a graph",
 "    TGraph *gr1 = new TGraph(nPoints, &time_stachiwCycle1[0], &temperature_stachiwCycle1[0]);",
+"    gr1->SetLineColor(kGray+1);// Line color",
+"    gr1->SetLineWidth(2);  // Line width",
 "    gr1->SetLineStyle(2);  // Dashed line",
-"    gr1->SetTitle(\"Annealing Cycle 1, raw single-layer\");",
+"    gr1->SetTitle(\"Annealing Cycle, raw single-layer\");",
 "    gr1->GetXaxis()->SetTitle(\"Hours\");",
 "    gr1->GetYaxis()->SetTitle(\"Temperature (^{#circ}C,\");",
 "",
